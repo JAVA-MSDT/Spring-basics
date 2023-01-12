be751a9c3bd051321bd6c044aefa99b67a256d52
Ahmed Samy <serenitydiver@hotmail.com>
2023-01-12 18:13:58 +0100

    Project Initializing

---
+++ Module 26 Tasks/Event Ticket Booking/src/main/java/org/javamsdt/Main.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2023 Ahmed Samy.
+ */
+
+package org.javamsdt;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
