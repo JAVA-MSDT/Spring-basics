2f1787dbb6a889fd3179935345fd0a2c49512dcf
Ahmed Samy <serenitydiver@hotmail.com>
2023-03-20 09:53:14 +0100

    fixed Flyway migration issue

--- rest/service-rest/src/main/resources/db/migration/V1.0__createTable.sql
+++ rest/service-rest/src/main/resources/db/migration/V1.0__createTable.sql
@@ -2,17 +2,17 @@
 ------------------- BUILD TABLES -------------------
 ----------------------------------------------------
 create table users (
-    id BBIGSERIAL PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
      birth_date DATE,
      name VARCHAR(255),
      surname VARCHAR(255)
 );
 
 create table subscription(
-    id BBIGSERIAL PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     start_date DATE,
     user_id BIGINT,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 ----------------------------------------------------
